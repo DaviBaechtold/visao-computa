a02cdd12b9bf9fa2c116788effa2022e134cd490
Vinicius Bregoli <vinibregoli@gmail.com>
2025-06-02 19:39:10 +0200

    Implementado: media, moda, desvio padrao, max, min, entropia e histograma

--- basics.py
+++ basics.py
@@ -4,21 +4,134 @@ Created on Mon May  3 19:18:29 2021
 
 @author: Davi Baechtold Campos, Vinicius Bregoli, Lincoln Rodrigo
 """
-from numba import njit # conda install numba
+from numba import njit  # conda install numba
+import numpy as np
+import math
+
 
 @njit
 def histogram_figure_numba(np_img):
-    '''
+    """
     Jit compiled function to increase performance.
     Use some loops insteads of purely numpy functions.
     If you face some compile errors using @njit, see: https://numba.pydata.org/numba-doc/dev/reference/numpysupported.html
     In case you dont need performance boosts, remove the njit flag above the function
     Do not use cv2 functions together with @njit
-    '''
-    
-    # return r_bars, g_bars, b_bars
-    return [0], [0], [0]
+    """
+    r_bars = [0] * 256
+    g_bars = [0] * 256
+    b_bars = [0] * 256
 
+    for j in range(np_img.shape[0]):
+        for k in range(np_img.shape[1]):
+            pixel = np_img[j, k]
+            redIntensity = int(pixel[0])
+            greenIntensity = int(pixel[1])
+            blueIntensity = int(pixel[2])
+            r_bars[redIntensity] += 1
+            g_bars[greenIntensity] += 1
+            b_bars[blueIntensity] += 1
+
+    return r_bars, g_bars, b_bars
+
+
+def calculate_channels_stats(r_bars, g_bars, b_bars):
+    # Mean = sum of intensities / n of pixels
+    r_mean = 0
+    g_mean = 0
+    b_mean = 0
+    for i in range(256):
+        r_mean += i * r_bars[i]
+        g_mean += i * g_bars[i]
+        b_mean += i * b_bars[i]
+    r_sum = sum(r_bars)
+    g_sum = sum(g_bars)
+    b_sum = sum(b_bars)
+    r_mean /= r_sum
+    g_mean /= g_sum
+    b_mean /= b_sum
+
+    # Mode = most common intensity
+    r_mode = r_bars.index(max(r_bars))
+    g_mode = g_bars.index(max(g_bars))
+    b_mode = b_bars.index(max(b_bars))
+
+    # Standard Deviation = sqrt(sum of (intensity - mean)^2 / n of pixels)
+    std_r = 0
+    std_g = 0
+    std_b = 0
+    for i in range(256):
+        std_r += (i - r_mean) ** 2 * r_bars[i]
+        std_g += (i - g_mean) ** 2 * g_bars[i]
+        std_b += (i - b_mean) ** 2 * b_bars[i]
+    std_r = (std_r / r_sum) ** 0.5
+    std_g = (std_g / g_sum) ** 0.5
+    std_b = (std_b / b_sum) ** 0.5
+
+    # Max and min
+    for i in range(255, -1, -1):
+        if r_bars[i] > 0:
+            r_max = i
+            break
+    for i in range(255, -1, -1):
+        if g_bars[i] > 0:
+            g_max = i
+            break
+    for i in range(255, -1, -1):
+        if b_bars[i] > 0:
+            b_max = i
+            break
+    for i in range(256):
+        if r_bars[i] > 0:
+            r_min = i
+            break
+    for i in range(256):
+        if g_bars[i] > 0:
+            g_min = i
+            break
+    for i in range(256):
+        if b_bars[i] > 0:
+            b_min = i
+            break
+
+    # Entropy = -sum of (p_i * log2(p_i))
+    r_entropy = 0
+    g_entropy = 0
+    b_entropy = 0
+    for i in range(256):
+        p_i_r = r_bars[i] / r_sum
+        p_i_g = g_bars[i] / g_sum
+        p_i_b = b_bars[i] / b_sum
+        if p_i_r > 0: # log2(0) is undefined
+            r_entropy += p_i_r * math.log2(p_i_r)
+        if p_i_g > 0:
+            g_entropy += p_i_g * math.log2(p_i_g)
+        if p_i_b > 0:
+            b_entropy += p_i_b * math.log2(p_i_b)
+    r_entropy = -r_entropy
+    g_entropy = -g_entropy
+    b_entropy = -b_entropy
+
+    return (
+        r_mean,
+        g_mean,
+        b_mean,
+        r_mode,
+        g_mode,
+        b_mode,
+        std_r,
+        std_g,
+        std_b,
+        r_max,
+        g_max,
+        b_max,
+        r_min,
+        g_min,
+        b_min,
+        r_entropy,
+        g_entropy,
+        b_entropy,
+    )
 
 
 ####
--- run.py
+++ run.py
@@ -5,11 +5,16 @@ Created on Thu Apr 22 11:59:19 2021
 @author: droes
 """
 # You can use this library for oberserving keyboard presses
-import keyboard # pip install keyboard
+import keyboard
 
 from capturing import VirtualCamera
-from overlays import initialize_hist_figure, plot_overlay_to_image, plot_strings_to_image, update_histogram
-from basics import histogram_figure_numba
+from overlays import (
+    initialize_hist_figure,
+    plot_overlay_to_image,
+    plot_strings_to_image,
+    update_histogram,
+)
+from basics import histogram_figure_numba, calculate_channels_stats
 
 
 # Example function
@@ -18,46 +23,73 @@ from basics import histogram_figure_numba
 def custom_processing(img_source_generator):
     # use this figure to plot your histogram
     fig, ax, background, r_plot, g_plot, b_plot = initialize_hist_figure()
-    
+    SHOW_HIST = True
+    SHOW_STATS = True
+
     for sequence in img_source_generator:
+        # sequence is a numpy array of shape (height, width, 3)
         # Call your custom processing methods here! (e. g. filters)
-        
-        
 
         # Example of keyboard is pressed
-        # If you want to use this method then consider implementing a counter
-        # that ignores for example the next five keyboard press events to
-        # "prevent" double clicks due to high fps rates
-        if keyboard.is_pressed('h') :
-            print('h pressed')
-            
+        if keyboard.is_pressed("h"):
+            SHOW_HIST = not SHOW_HIST
+        if keyboard.is_pressed("s"):
+            SHOW_STATS = not SHOW_STATS
 
-        ###
-        ### Histogram overlay example (without data)
-        ###
-        
         # Load the histogram values
-        r_bars, g_bars, b_bars = histogram_figure_numba(sequence)        
-        
+        if SHOW_HIST:
+            r_bars, g_bars, b_bars = histogram_figure_numba(sequence)
+        if SHOW_STATS:
+            # Calculate the stats
+            (
+                r_mean,
+                g_mean,
+                b_mean,
+                r_mode,
+                g_mode,
+                b_mode,
+                std_r,
+                std_g,
+                std_b,
+                r_max,
+                g_max,
+                b_max,
+                r_min,
+                g_min,
+                b_min,
+                r_entropy,
+                g_entropy,
+                b_entropy,
+            ) = calculate_channels_stats(r_bars, g_bars, b_bars)
+
+        # Normalize them between 0 and 3
+        if SHOW_HIST:
+            r_bars = [i * 3 / max(r_bars) for i in r_bars]
+            g_bars = [i * 3 / max(g_bars) for i in g_bars]
+            b_bars = [i * 3 / max(b_bars) for i in b_bars]
+
         # Update the histogram with new data
-        update_histogram(fig, ax, background, r_plot, g_plot, b_plot, r_bars, g_bars, b_bars)
-        
-        # uses the figure to create the overlay
-        sequence = plot_overlay_to_image(sequence, fig)
-        
-        ###
-        ### END Histogram overlay example
-        ###
+        if SHOW_HIST:
+            update_histogram(
+                fig, ax, background, r_plot, g_plot, b_plot, r_bars, g_bars, b_bars
+            )
+            # uses the figure to create the overlay
+            sequence = plot_overlay_to_image(sequence, fig)
 
-        
         # Display text example
-        display_text_arr = ["Test", "abc"]
-        sequence = plot_strings_to_image(sequence, display_text_arr)
+        if SHOW_STATS:
+            display_text_arr = [
+                f"Mean(RGB): ({r_mean:.2f}, {g_mean:.2f}, {b_mean:.2f})",
+                f"Mode(RGB): ({r_mode}, {g_mode}, {b_mode})",
+                f"Std(RGB): ({std_r:.2f}, {std_g:.2f}, {std_b:.2f})",
+                f"Max(RGB): ({r_max}, {g_max}, {b_max})",
+                f"Min(RGB): ({r_min}, {g_min}, {b_min})",
+                f"Entropy(RGB): ({r_entropy:.2f}, {g_entropy:.2f}, {b_entropy:.2f})",
+            ]
+            sequence = plot_strings_to_image(sequence, display_text_arr)
 
-        
         # Make sure to yield your processed image
         yield sequence
-
 
 
 def main():
@@ -65,19 +97,19 @@ def main():
     width = 1280
     height = 720
     fps = 30
-    
+
     # Define your virtual camera
     vc = VirtualCamera(fps, width, height)
-    
+
     vc.virtual_cam_interaction(
         custom_processing(
             # either camera stream
             vc.capture_cv_video(0, bgr_to_rgb=True)
-            
             # or your window screen
             # vc.capture_screen()
         )
     )
+
 
 if __name__ == "__main__":
     main()
